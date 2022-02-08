4ebf116e8d0af5aaf96fe61ad3c1c388fd68ddcc
code-pil <vaghelapiyush3132@gmail.com>
2022-02-08 14:48:17 +0530

    Added about page

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function About() {
+  return (
+      <>
+        <h1>About</h1>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Pellentesque euismod, urna eu tincidunt consectetur,
+            nisi nisl aliquam nunc, eget consectetur nunc nisi
+            vitae nisl.
+        </p>
+      </>
+  );
+}
